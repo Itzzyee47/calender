ee7b82b4c87467ee61d21daacdc289fb1d2abd64
Itzzyee47 <111084815+Itzzyee47@users.noreply.github.com>
2024-04-06 09:37:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from oauth2client import file, client, tools
+from service import service
 
 app = Flask(__name__)
 
@@ -20,53 +21,54 @@ app = Flask(__name__)
 def index():
 
     if request.method == "POST":
-        try:
-            import argparse
-            flags = argparse.ArgumentParser().parse_args()
-        except ImportError:
-            flags = None
-        SCOPES = ['https://www.googleapis.com/auth/calendar']
-        store = file.Storage('storage.json')
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-            creds = tools.run_flow(flow, store, flags) \
-                    if flags else tools.run(flow, store)
-        CAL = build('calendar', 'v3', http=creds.authorize(Http()))
-        event = {
-            'summary': 'Google Calendar API trys',
-            'location': '800 Howard St., San Francisco, CA 94103',
-            'description': 'A reminder of the test use of googles calender api.',
-            'start': {
-                'dateTime': '2015-05-28T09:00:00-07:00',
-                'timeZone': 'America/Los_Angeles',
-            },
-            'end': {
-                'dateTime': '2015-05-28T17:00:00-07:00',
-                'timeZone': 'America/Los_Angeles',
-            },
-            'recurrence': [
-                'RRULE:FREQ=DAILY;COUNT=2'
-            ],
-            'attendees': [
-                {'email': 'ebongloveis@gmail.com'},
-                {'email': 'joeltabe3@gmail.com'},
-            ],
-            'reminders': {
-                'useDefault': False,
-                'overrides': [
-                {'method': 'email', 'minutes': 24 * 60},
-                {'method': 'popup', 'minutes': 10},
-                ],
-            },
-            }
+        service()
+        # try:
+        #     import argparse
+        #     flags = argparse.ArgumentParser().parse_args()
+        # except ImportError:
+        #     flags = None
+        # SCOPES = ['https://www.googleapis.com/auth/calendar']
+        # store = file.Storage('storage.json')
+        # creds = store.get()
+        # if not creds or creds.invalid:
+        #     flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        #     creds = tools.run_flow(flow, store, flags) \
+        #             if flags else tools.run(flow, store)
+        # CAL = build('calendar', 'v3', http=creds.authorize(Http()))
+        # event = {
+        #     'summary': 'Google Calendar API trys',
+        #     'location': '800 Howard St., San Francisco, CA 94103',
+        #     'description': 'A reminder of the test use of googles calender api.',
+        #     'start': {
+        #         'dateTime': '2015-05-28T09:00:00-07:00',
+        #         'timeZone': 'America/Los_Angeles',
+        #     },
+        #     'end': {
+        #         'dateTime': '2015-05-28T17:00:00-07:00',
+        #         'timeZone': 'America/Los_Angeles',
+        #     },
+        #     'recurrence': [
+        #         'RRULE:FREQ=DAILY;COUNT=2'
+        #     ],
+        #     'attendees': [
+        #         {'email': 'ebongloveis@gmail.com'},
+        #         {'email': 'joeltabe3@gmail.com'},
+        #     ],
+        #     'reminders': {
+        #         'useDefault': False,
+        #         'overrides': [
+        #         {'method': 'email', 'minutes': 24 * 60},
+        #         {'method': 'popup', 'minutes': 10},
+        #         ],
+        #     },
+        #     }
 
-        e = CAL.events().insert(calendarId='primary', sendNotifications=True, body=event).execute()
-        if e:
-            context = {"link":event.get('htmlLink')}
-            return render_template('index.html',context)
+        # e = CAL.events().insert(calendarId='primary', sendNotifications=True, body=event).execute()
+        # if e:
+        #     context = {"link":event.get('htmlLink')}
+        #     return render_template('index.html',context)
         
-        REDIRECT_URI = "https://calender-quf5.onrender.com/"
+        # REDIRECT_URI = "https://calender-quf5.onrender.com/"
         
     return render_template('index.html')
     
