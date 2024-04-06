30c4482cee97581d0a81e051536282cfb09640db
Itzzyee47 <111084815+Itzzyee47@users.noreply.github.com>
2024-04-06 07:58:41 -0700

    added service acount app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pickle
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
